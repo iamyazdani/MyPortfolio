1d662686b520e6f63351d0361b6e5b0f988f5f14
Gulam Yazdani <gulamyazdani03@gmail.com>
2023-11-30 22:26:26 +0530

    Updating cv

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -100,7 +100,7 @@ const Experience = () => {
                         <article className="experience__details">
                             <BsPatchCheckFill className="experience__details-icon" />
                             <div>
-                                <h4>Wordpress</h4>
+                                <h4>WordPress</h4>
                                 <small className="text-light">Intermediate</small>
                             </div>
                         </article>
